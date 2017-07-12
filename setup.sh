c941141ffb52516453271c8eb904f80db9890b4a
KellyLampkin <kellye.lampkin@yahoo.com>
2017-07-11 19:22:19 -0500

    update sh

--- setup.sh
+++ setup.sh
@@ -16,7 +16,8 @@ echo "export PATH=$HOME/bin:$PATH" >> ~/.bashrc
 
 # Step 4
 # Uncomment the line below and make sure you provide your own API Key, see http://exercism.io/cli/linux
-exercism configure --key=8164ec9f238e44baab0ad3e54fa4162c
+exercism configure --key=5f14a2d575f849628204da4e021bef3a
+
 
 # Step 5
 # You can change this location but not recommended, DO NOT CHANGE! unless you know what you are doing
